0bffa105ff07431af48824cd8ef8f22b4c8e0903
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-03-30 10:37:59 +0700

    [learn-fun-ajax-b01] 03. Basic using AJAX loading json and api data b01

--- Fundamental/ajaxTutorialB01/01_AJAX_Basics/app.js
+++ Fundamental/ajaxTutorialB01/01_AJAX_Basics/app.js
@@ -1,3 +1,4 @@
+// display data text
 var displayTextDataFunc = {
     // Text file data
     init: function () {
@@ -29,6 +30,83 @@ var displayTextDataFunc = {
     },
 }
 
+// display json data
+var displayJsonDataFunc = {
+    init: function () {
+        let jsonBtn = document.querySelector('#json-btn');
+        jsonBtn.addEventListener('click', function () {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', './data/mobiles.json', true);
+            xhr.send();
+            xhr.onload = () => {
+                let data = xhr.response;
+
+                let mobile = JSON.parse(data);
+                console.log(mobile);
+                console.log(typeof mobile);
+                displayJsonDataFunc.displayJsonObjectData(mobile);
+            }
+        });
+    },
+
+    // display json data
+    displayJsonObjectData: function (mobile) {
+        var htmlTemplate = `
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${mobile.id}</li>
+                <li class="list-group-item">BRAND: ${mobile.brand}</li>
+                <li class="list-group-item">COLOR: ${mobile.color}</li>
+                <li class="list-group-item">PRICE: ${mobile.price}</li>
+            </ul>
+        `;
+
+        document.querySelector("#json-card").innerHTML = htmlTemplate;
+    }
+}
+
+// display API data
+var displayApiDataFunc = {
+    init: function () {
+        let apiBtn = document.querySelector('#api-btn');
+        apiBtn.addEventListener('click', function () {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+            xhr.send();
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    let data = xhr.response;
+                    // console.log(data);
+                    let users = JSON.parse(data);
+                    console.log(users);
+                    displayApiDataFunc.displayAPIData(users);
+                }
+            }
+        });
+    },
+
+    // display list data on API response
+    displayAPIData: function (users) {
+        var htmlTemplate = ``;
+        for (let user of users) {
+            htmlTemplate += `
+                <ul class='list-group mt-1'>
+                    <li class="list-group-item">ID:    ${user.id}</li>
+                    <li class="list-group-item">USER NAME: ${user.name}</li>
+                    <li class="list-group-item">STREET: ${user.address.street}</li>
+                    <li class="list-group-item">CITY:   ${user.address.city}</li>
+                    <li class="list-group-item">ZIP CODE:   ${user.address.zipcode}</li>
+                </ul>
+            `;
+        }
+
+        document.querySelector('#api-card').innerHTML = htmlTemplate;
+    }
+}
+
+
+
 $(function () {
     displayTextDataFunc.init();
+    displayJsonDataFunc.init();
+    displayApiDataFunc.init();
 })
