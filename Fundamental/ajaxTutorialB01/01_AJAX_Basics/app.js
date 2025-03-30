bde02bc3de828b598f3d02b680077ff937c018a3
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-03-30 09:32:54 +0700

    [learn-fun-ajax-b01] 02. Basic using AJAX loading text data b01

--- Fundamental/ajaxTutorialB01/01_AJAX_Basics/app.js
+++ Fundamental/ajaxTutorialB01/01_AJAX_Basics/app.js
@@ -1,1 +1,34 @@
-console.log('Welocme to AJAX');
+var displayTextDataFunc = {
+    // Text file data
+    init: function () {
+        let textBtn = document.querySelector('#text-btn');
+        textBtn.addEventListener('click', function () {
+            // Create AJAX request
+            let xhr = new XMLHttpRequest();
+
+            // Prepare the request
+            xhr.open('GET', './data/message.txt', true);
+
+            // send the request
+            xhr.send();
+
+            // Process the request
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    let data = xhr.responseText;
+                    console.log(data);
+                    displayTextDataFunc.displayTextData(data);
+                }
+            }
+        })
+    },
+
+    displayTextData: function (data) {
+        var htmlTemplate = `<h3>${data}</h3>`;
+        document.querySelector('#text-card').innerHTML = htmlTemplate;
+    },
+}
+
+$(function () {
+    displayTextDataFunc.init();
+})
